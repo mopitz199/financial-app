bf5e6120e20769f8a32e042417d66eff3b0b591f
Maximiliano Opitz Turra <maximilianoopitzturra@MacBook-Pro-de-Maximiliano.local>
2019-07-31 00:00:03 -0400

    chore(financial_app): new form to simulate the profitability of a appartment

--- src/App.js
+++ src/App.js
@@ -106,7 +106,7 @@ export default function MiniDrawer() {
   const classes = useStyles();
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
-  const [currentPage, setCurrentPage] = React.useState(pages[2]);
+  const [currentPage, setCurrentPage] = React.useState(pages[0]);
 
   const [irpdData, setIrpdData] = useState([])
   const [nationalOfferData, setNationalOfferData] = useState([])
